05d0f4cccdee10261a86aae9237fbabad8b55dd1
Min-soo Park <rinirihiriro@gmail.com>
2015-02-28 18:37:40 +0900

    불필요한 코드 제거

--- src/AntManager/Classes/Ant.cpp
+++ src/AntManager/Classes/Ant.cpp
@@ -9,7 +9,6 @@
 USING_NS_CC;
 
 Ant::Ant()
-	:m_MoveCount(0)
 {
 
 }
@@ -25,11 +24,9 @@ bool Ant::init()
 	{
 		return false;
 	}
-	makeDirVector();
 	makeSprite();
 	scheduleUpdate();
 	schedule(schedule_selector(Ant::decideNextDirection), 0.5f);
-//	initProbability();
 	m_Velocity = Vec2::UNIT_Y * (rand() % 10 + 20);
 	return true;
 }
@@ -37,49 +34,9 @@ bool Ant::init()
 void Ant::update(float dTime)
 {
 	updatePosition(dTime);
-
-	auto pos = getPosition();
-
-	// 맵 밖으로 못 나가게
-	if (pos.x < 0)
-		pos.x = 0;
-	else if (pos.x > 480)
-		pos.x = 480;
-	if (pos.y < 0)
-		pos.y = 0;
-	else if (pos.y > 640)
-		pos.y = 640;
-
-	setPosition(pos);
-
-	MapModel* mapModel = GET_GAME_MANAGER()->getMachine()->getMapModel();
-	int xIdx = pos.x / TILE_SIZE_WIDTH;
-	int yIdx = pos.y / TILE_SIZE_HEIGHT;
-	int checkIdx = xIdx + yIdx * mapModel->getWidth();
-	if (mapModel->getData(checkIdx) != 0)
-	{
-		float dx = (xIdx + 0.5f) * TILE_SIZE_WIDTH;
-		float dy = (yIdx + 0.5f) * TILE_SIZE_HEIGHT;
-		if (abs(dx - pos.x) > abs(dy - pos.y))
-		{
-			if (dx > pos.x)
-				pos.x = dx - TILE_SIZE_WIDTH * 0.6;
-			else
-				pos.x = dx + TILE_SIZE_WIDTH * 0.6;
-		}
-		else
-		{
-			if (dy > pos.y)
-				pos.y = dy - TILE_SIZE_WIDTH * 0.6;
-			else
-				pos.y = dy + TILE_SIZE_WIDTH * 0.6;
-		}
-		setPosition(pos);
-	}
-
-//	decideNextDirection(dTime);
-	// move(dTime);
+	wallProcess();
 }
+
 
 void Ant::decideNextDirection(float dTime)
 {
@@ -133,101 +90,50 @@ void Ant::decideNextDirection(float dTime)
 		);
 }
 
-void Ant::move(float dTime)
-{
-	if(m_MoveCount++ > 5)
-	{
-		m_MoveCount = 0;
-		MapModel* mapModel = GET_GAME_MANAGER()->getMachine()->getMapModel();
-		Rect fieldRect(0, 0, 480, 640);
-		int moveDirection = findBestDir();
-		Vec2 moveVec = m_DirVector[moveDirection] * dTime * 300;
-		Point nextPos = getPosition() + moveVec;
-		int xIdx = nextPos.x / TILE_SIZE_WIDTH;
-		int yIdx = nextPos.y / TILE_SIZE_HEIGHT;
-		int checkIdx = xIdx + yIdx * mapModel->getWidth();
-		while(!fieldRect.containsPoint(nextPos) || mapModel->getData(checkIdx))
-		{
-			m_DirProbability[moveDirection]--;
-			moveDirection = rand() % MD_MAX;
-			moveVec = m_DirVector[moveDirection] * dTime * 300;
-			nextPos = getPosition() + moveVec;
-			xIdx = nextPos.x / TILE_SIZE_WIDTH;
-			yIdx = nextPos.y / TILE_SIZE_HEIGHT;
-			checkIdx = xIdx + yIdx * mapModel->getWidth();
-			if(isNoWayToGo())
-			{
-				removeFromParent();
-				return;
-			}
-		}
-		initProbability();
-		setPosition(nextPos);
-	}
-	return;
-}
-
-void Ant::makeDirVector()
-{
-	m_DirVector[MD_UP] = Vec2(0, 1);
-	m_DirVector[MD_UP_LEFT] = Vec2(-1, 1);
-	m_DirVector[MD_UP_RIGHT] = Vec2(1, 1);
-	m_DirVector[MD_LEFT] = Vec2(-1, 0);
-	m_DirVector[MD_RIGHT] = Vec2(1, 0);
-
-	
-}
-
 void Ant::makeSprite()
 {
 	m_Sprite = Sprite::create("ant_black.png");
 	addChild(m_Sprite);
 }
 
-void Ant::initProbability()
+void Ant::wallProcess()
 {
-	m_DirProbability[MD_NONE] = 0;
-	m_DirProbability[MD_LEFT] = 10;
-	m_DirProbability[MD_RIGHT] = 10;
-	m_DirProbability[MD_UP] = 11;
-	m_DirProbability[MD_UP_LEFT] = 11;
-	m_DirProbability[MD_UP_RIGHT] = 11;
-}
+	auto pos = getPosition();
 
-bool Ant::isNoWayToGo()
-{
-	bool ret = true;
-	for(int i = 0; i < MD_MAX; ++i)
-	{
-		if(m_DirProbability[i] > 0)
-		{
-			ret = false;
-		}
-	}
-	return ret;
-}
+	// 맵 밖으로 못 나가게
+	if (pos.x < 0)
+		pos.x = 0;
+	else if (pos.x > 480)
+		pos.x = 480;
+	if (pos.y < 0)
+		pos.y = 0;
+	else if (pos.y > 640)
+		pos.y = 640;
 
-int Ant::findBestDir()
-{
-	std::vector<int> bestDirs;
-	int bestProbability = 0;
-	int bestDir = MD_NONE;
-	int checkProbability = 0;
-	for(int dir = 0; dir < MD_MAX; ++dir)
+	setPosition(pos);
+
+	MapModel* mapModel = GET_GAME_MANAGER()->getMachine()->getMapModel();
+	int xIdx = pos.x / TILE_SIZE_WIDTH;
+	int yIdx = pos.y / TILE_SIZE_HEIGHT;
+	int checkIdx = xIdx + yIdx * mapModel->getWidth();
+	if (mapModel->getData(checkIdx) != 0)
 	{
-		checkProbability = m_DirProbability[dir];
-		if( checkProbability > bestProbability)
+		float dx = (xIdx + 0.5f) * TILE_SIZE_WIDTH;
+		float dy = (yIdx + 0.5f) * TILE_SIZE_HEIGHT;
+		if (abs(dx - pos.x) > abs(dy - pos.y))
 		{
-			bestProbability = checkProbability;
-			bestDir = dir;
-			bestDirs.clear();
-			bestDirs.push_back(dir);
+			if (dx > pos.x)
+				pos.x = dx - TILE_SIZE_WIDTH * 0.6;
+			else
+				pos.x = dx + TILE_SIZE_WIDTH * 0.6;
 		}
-		else if(checkProbability == bestProbability)
+		else
 		{
-			bestDirs.push_back(dir);
+			if (dy > pos.y)
+				pos.y = dy - TILE_SIZE_WIDTH * 0.6;
+			else
+				pos.y = dy + TILE_SIZE_WIDTH * 0.6;
 		}
+		setPosition(pos);
 	}
-	
-	return bestDirs[rand() % bestDirs.size()];
 }
--- src/AntManager/Classes/Ant.h
+++ src/AntManager/Classes/Ant.h
@@ -11,31 +11,11 @@ public:
 
 	CREATE_FUNC(Ant);
 
-public:
-	enum MoveDirection
-	{
-		MD_NONE,
-		MD_UP,
-		MD_UP_LEFT,
-		MD_LEFT,
-		MD_UP_RIGHT,
-		MD_RIGHT,
-		MD_MAX,
-	};
+private:
+	void	makeSprite();
+	void	decideNextDirection(float dTime);
+	void	wallProcess();
 
 private:
-	void			move(float dTime);
-	void			makeDirVector();
-	void			makeSprite();
-	void			initProbability();
-	bool			isNoWayToGo();
-	int				findBestDir();
-
-	void decideNextDirection(float dTime);
-
-private:
-	int									m_MoveCount;
-	std::array<cocos2d::Vec2, MD_MAX>	m_DirVector;
-	std::array<int, MD_MAX>				m_DirProbability;
-	cocos2d::Sprite*					m_Sprite;
+	cocos2d::Sprite*	m_Sprite;
 };
